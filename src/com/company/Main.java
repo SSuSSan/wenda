febe49580df3cc99aced5cd44e832364b59c4b9a
SSuSSan <3371274008@qq.com>
2018-06-03 11:26:22 +0800

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        StringBuffer s = new StringBuffer("he likes Java");
-        String s;
-        s.charAt(10);
-        System.out.println(s);
-        s.setCharAt(0, 'w');
-        System.out.p;
-        System.out.print();
-        System.out.println(s);
-        s.insert(3, "all ");
-
-        System.out.println(s);
-        s.replace(13, 17, "apple");
-
-        System.out.println(s);
-//        int index = s.indexOf("apple");
-//        s.replace(index, s.length(), "play");//不用去数，用indexOf办法，Low了吧
+        System.out.print("hello");
+        System.out.print("hello1");
     }
 }
